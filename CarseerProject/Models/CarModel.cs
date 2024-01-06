936ff70ee625826e3bc7e202e3915a7fe782ef60
Mohammad Ibrahim Abu Dalleh <M.Dalleh.ext@tahaluf.ai>
2024-01-07 02:23:06 +0300

    add model

--- CarseerProject/Models/CarModel.cs
+++ CarseerProject/Models/CarModel.cs
@@ -1,6 +1,18 @@
 ﻿namespace CarseerProject.Models
 {
+    public class CarModelsResponse
+    {
+        public int Count { get; set; }
+        public string Message { get; set; }
+        public string SearchCriteria { get; set; }
+        public List<CarModel> Results { get; set; }
+    }
+
     public class CarModel
     {
+        public int Make_ID { get; set; }
+        public string Make_Name { get; set; }
+        public int Model_ID { get; set; }
+        public string Model_Name { get; set; }
     }
 }
